fa1dae2f5938c8ed20f09b3924ca5804cf816fde
DanielQFK <DanialQueen2004@gmail.com>
2021-08-26 01:04:53 -0400

    ...

---
+++ Extra-Learning-Python-03.py
@@ -0,0 +1,12 @@
+# This program is a simple way that you can do something to make "for" act like while
+# I mean , to make "for " not to stop and continues ...
+# Make a list to be able to put it after "for x in" 
+List = [1 , 2]
+
+# Then write the "for" part...
+for x in List:  
+    a = input("Enter > ")#we write it to control the for , if we don't it print unlimited...
+    # and write something to append a number in your list in each movement    
+    List.append(1)
+
+    # As you see it append a number again and again , so x never reaches to our list 
